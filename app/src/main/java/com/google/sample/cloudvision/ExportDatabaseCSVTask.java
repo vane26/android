73bdf7586a4e7b0a19b1e8209195f20fe1a5fef7
vane26 <vane.ccordovaz@gmail.com>
2016-09-13 15:40:12 -0300

    AHORA SI

--- app/src/main/java/com/google/sample/cloudvision/ExportDatabaseCSVTask.java
+++ app/src/main/java/com/google/sample/cloudvision/ExportDatabaseCSVTask.java
@@ -1,6 +1,8 @@
 package com.google.sample.cloudvision;
 
+import android.app.ProgressDialog;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Environment;
@@ -16,11 +18,22 @@ import java.io.IOException;
  * Created by Vane on 13/09/2016.
  */
 public class ExportDatabaseCSVTask extends AsyncTask<String, Void, Boolean> {
-    public static SQLiteDatabase simpledb = null;
+    public static SQLiteDatabase simpledb;
+
+    private android.content.Context ctx;
+    private final ProgressDialog dialog = new ProgressDialog(ctx);
+    // can use UI thread here
+    @Override
+    protected void onPreExecute()
+    {
+        this.dialog.setMessage("Exporting database...");
+        this.dialog.show();
+    }
 
     protected Boolean doInBackground(final String... args) {
         String currentDBPath = "/data/data/com.google.sample.cloudvision.BD/databases/registro_db";
-        File dbFile = getDatabasePath("" + currentDBPath);
+        File dbFile=getDatabasePath("registro_db");
+
         System.out.println(dbFile);  // displays the data base path in your logcat
         File exportDir = new File(Environment.getExternalStorageDirectory(), "/sdcard/registro_db");
 
@@ -35,23 +48,27 @@ public class ExportDatabaseCSVTask extends AsyncTask<String, Void, Boolean> {
 
             Cursor curCSV = simpledb.rawQuery("select * from " + registroContract.registroEntry.table_name, null);
             csvWrite.writeNext(curCSV.getColumnNames());
-            while (curCSV.moveToNext()) {
-                String arrStr[] = null;
-                String[] mySecondStringArray = new String[curCSV.getColumnNames().length];
-                for (int i = 0; i < curCSV.getColumnNames().length; i++) {
-                    mySecondStringArray[i] = curCSV.getString(i);
-                }
-                csvWrite.writeNext(mySecondStringArray);
+            while(curCSV.moveToNext())
+            {
+                String arrStr[] ={curCSV.getString(0),curCSV.getString(1),
+                        curCSV.getString(2),curCSV.getString(3)};
+                csvWrite.writeNext(arrStr);
             }
             csvWrite.close();
             curCSV.close();
             return true;
-        } catch (IOException e) {
+        }
+        catch(SQLException sqlEx)
+        {
+            Log.e("MainActivity", sqlEx.getMessage(), sqlEx);
+            return false;
+        }
+        catch (IOException e)
+        {
             Log.e("MainActivity", e.getMessage(), e);
             return false;
         }
     }
-
     public File getDatabasePath(String s) {
         return getDatabasePath(s);
     }
--- app/src/main/java/com/google/sample/cloudvision/MainActivity.java
+++ app/src/main/java/com/google/sample/cloudvision/MainActivity.java
@@ -329,5 +329,4 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
 }
